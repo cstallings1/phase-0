850e6cf178fe2cf9395838bc653cfaa7c069c760
cstallings1 <catiestallings1@gmail.com>
2015-12-02 18:44:54 -0600

    Add solution files for challenge 4.6.5

---
+++ week-4/simple-substrings/11-simple-substrings/my_solution.rb
@@ -0,0 +1,13 @@
+# Simple Substrings
+
+# I worked on this challenge [with: Liam Mackey].
+
+
+# Your Solution Below
+def welcome(phrase)
+  if phrase.include?("CA")
+    return "Welcome to California"
+  else
+    return "You should move to California"
+  end
+end
