6b5b1bc3ec0de6ba31b96f0e99de26410ac7649b
cstallings1 <catiestallings1@gmail.com>
2015-12-02 18:43:59 -0600

    Add solution files for challenge 4.6.1

---
+++ week-4/good-guess/7-good-guess/my_solution.rb
@@ -0,0 +1,13 @@
+# Good Guess
+
+# I worked on this challenge [with Liam Mackey].
+
+
+# Your Solution Below
+def good_guess?(num)
+  if num == 42
+    return true
+  else
+    return false
+  end
+end
