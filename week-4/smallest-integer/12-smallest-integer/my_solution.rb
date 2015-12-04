5dae652f954e91c4396885e8eedae6799f3603c9
cstallings1 <catiestallings1@gmail.com>
2015-12-04 10:12:23 -0600

    Added initial solution

--- week-4/smallest-integer/12-smallest-integer/my_solution.rb
+++ week-4/smallest-integer/12-smallest-integer/my_solution.rb
@@ -12,6 +12,20 @@
 
 # Your Solution Below
 def smallest_integer(list_of_nums)
+  min = list_of_nums[0]
+
+  list_of_nums.each do |num|
+    if num < min
+      min = num
+    end
+  end
+
+  return min
+
+end
+
+#Refactored:
+def smallest_integer(list_of_nums)
   list_of_nums.sort!
   return list_of_nums[0]
 end
