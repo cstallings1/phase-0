7ac540683f59c98993a8c058e9f1c2c09740815f
cstallings1 <catiestallings1@gmail.com>
2015-12-07 11:48:50 -0600

    Add refactored solution

--- week-5/nums-commas/nums-commas-solo-challenge/my_solution.rb
+++ week-5/nums-commas/nums-commas-solo-challenge/my_solution.rb
@@ -13,13 +13,17 @@
 # What is the output? (i.e. What should the code return?)
   #Initial number string with commas added appropriately
 # What are the steps needed to solve the problem?
-  # Create a new container
-  # WHILE digits remain
-  #   Put last 3 digits in the new container
-  #   Put a comma in the new container
+  # Split up each digit in original number
+  # Create a new container for the new number
+  # Create a counter
+
+  # WHILE original number is not empty
+  #   Take last digit of original number and add as first digit in the new number
+  #   Every third digit, add a comma to the beginning of the new container
   #   Repeat until no digits remain
   # END
-  # Return the new container
+
+  # Return the new container as a string
 
 
 # 1. Initial Solution
@@ -46,8 +50,18 @@ def separate_comma(num)
 end
 
 # 2. Refactored Solution
+def separate_comma(num)
+  num = num.to_s.split(//)
+  count = 0
 
+  (num.length / 3).times do |n|
+    count -= 4
+    num.insert(count, ",")
+  end
 
+  num.shift if num[0] == ","
 
+  return num.join("")
+end
 
 # 3. Reflection
