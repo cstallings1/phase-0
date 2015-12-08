8078b3f1b9c43f49938b63328ddf89ffab2d46d7
cstallings1 <catiestallings1@gmail.com>
2015-12-07 21:46:06 -0600

    Add refactored solution

--- week-5/gps2_2.rb
+++ week-5/gps2_2.rb
@@ -1,13 +1,33 @@
+# Method to print a list and make it look pretty
+# input: grocery list
+# steps:
+  #Puts each key value pair to the console formatted with a colon and space between key and value
+# output: Formatted grocery list
+
+#INITIAL SOLUTION
+def print_list(list)
+  list.each {|k,v| puts "#{k}: #{v}"}
+end
+
+#REFACTORED SOLUTION
+def print_list(list)
+  puts "---------------"
+  puts "Grocery List:"
+  puts "---------------"
+  list.each {|k,v| puts "#{k}: #{v}"}
+end
+
 # Method to create a list
-# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# input: string of items separated by spaces (example: "carrots apples  cereal pizza")
 # steps:
   # Create container
   # Add each word to the container, each word is separated by a space
   # Set each word as a key in a hash
   # set default quantity of each item to 1
   # print the list to the console [use print method..to be created]
-  # output: [output data will be a hash]
+# output: output data will be a hash
 
+#INITIAL SOLUTION
 def create_list(items, qty=1)
   items = items.split(" ")
   list = Hash.new
@@ -23,19 +43,29 @@ def create_list(items, qty=1)
   return list.each {|k,v| puts "#{k}: #{v}"}
 end
 
-puts create_list("carrots carrots apples apples cereal pizza")
+#REFACTORED SOLUTION
+def create_list(items, qty=1)
+  items = items.downcase.split(" ")
+  list = Hash.new
 
+  items.each do |item|
+    list.has_key?(item) ? list[item] += qty : list[item] = qty
+  end
 
+  print_list(list)
+end
 
+grocery_list = create_list("")
 
 # Method to add an item to a list
 # input: grocery list, item name, and optional quantity
 # steps:
   #Add new item and qty or default qty 1  as key and value to existing grocery list hash
   # If new item already exists in list do not add that item
-  # Print the list to the console
-# output: revised grocery list
+  # Print added items to the console
+# output: added items
 
+#INITIAL SOLUTION
 def add_item(list, item, qty=1)
   if !list.has_key?(item)
     list[item] = qty
@@ -44,28 +74,39 @@ def add_item(list, item, qty=1)
   return list.each {|k,v| puts "#{k}: #{v}"}
 end
 
-grocery_list = create_list("carrots carrots apples apples cereal pizza")
+#REFACTORED SOLUTION
+def add_item(list, item, qty=1)
+  if !list.has_key?(item)
+    list[item] = qty
+  end
+  puts "#{item} has been added."
+end
 
-puts add_item(grocery_list, "celery")
-
+add_item(grocery_list, "Lemonade", 2)
+add_item(grocery_list, "Tomato", 3)
+add_item(grocery_list, "Onion", 1)
+add_item(grocery_list, "Ice Cream", 4)
 
 # Method to remove an item from the list
 # input: grocery list, item name
 # steps:
   #Remove item from existing grocery list hash
-  #Print the revised list to the console
-# output: revised grocery list
+  #Print removed items to console
+# output: removed items
 
+#INITIAL SOLUTION
 def remove_item(list, item)
   list.delete(item)
   return list.each {|k,v| puts "#{k}: #{v}"}
 end
 
-grocery_list = create_list("carrots carrots apples apples cereal pizza")
+#REFACTORED SOLUTION
+def remove_item(list, item)
+  list.delete(item)
+  puts "#{item} has been removed."
+end
 
-puts remove_item(grocery_list, "carrots")
-
-
+remove_item(grocery_list, "Lemonade")
 
 # Method to update the quantity of an item
 # input: grocery list, item name, quantity
@@ -73,8 +114,9 @@ puts remove_item(grocery_list, "carrots")
   #Find the item in the original grocery list
   #Update the value of that key to use the new quantity
   #If item is not in the original list, do not update the quantity. Don't add to list
-# output: revised grocery list
+# output: updated item
 
+# INITIAL SOLUTION
 def update_qty(list, item, qty)
   if list.has_key?(item)
     list[item] = qty
@@ -82,22 +124,16 @@ def update_qty(list, item, qty)
   return list.each {|k,v| puts "#{k}: #{v}"}
 end
 
-grocery_list = create_list("carrots carrots apples apples cereal pizza")
-
-puts update_qty(grocery_list, "celery", 3)
-
-
-# Method to print a list and make it look pretty
-# input: grocery list
-# steps:
-  #Puts each key value pair to the console formatted with a colon and space between key and value
-# output: Formatted grocery list
-
-def print_list(list)
-  list.each {|k,v| return "#{k}: #{v}"}
+# #REFACTORED SOLUTION
+def update_qty(list, item, qty)
+  if list.has_key?(item)
+    list[item] = qty
+  end
+  puts "#{item} has been updated."
 end
 
-grocery_list = create_list("carrots carrots apples apples cereal pizza")
+update_qty(grocery_list, "Ice Cream", 1)
 
-puts print_list(grocery_list)
+print_list(grocery_list)
+
 
