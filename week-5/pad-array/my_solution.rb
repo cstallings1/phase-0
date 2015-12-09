ae06c2090ca0382272841b146cab69c4886d5665
cstallings1 <catiestallings1@gmail.com>
2015-12-08 18:13:23 -0600

    Add initial solution

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -20,13 +20,50 @@
 
 # 1. Initial Solution
 def pad!(array, min_size, value = nil) #destructive
-
+  new_array = []
+  if array.length >= min_size || min_size == 0
+    return array
+  else
+    new_array = Array.new(min_size - array.length, value)
+    final_array = array + (new_array)
+  end
+  return final_array
 end
 
 def pad(array, min_size, value = nil) #non-destructive
+  new_array = []
+  if array.length >= min_size || min_size == 0
+    return array.clone
+  else
+    new_array = Array.new(min_size - array.length, value)
+    final_array = array + new_array
+  end
+  return final_array
 end
 
 # 3. Refactored Solution
+def pad!(array, min_size, value = nil) #destructive
+   if array.length >= min_size || min_size == 0
+     return array
+   else
+     array.insert(array.last, *Array.new([ min_size - array.length].max, value))
+   end
+   return array
+ end
+
+ def pad(array, min_size, value = nil) #non-destructive
+   final_array = []
+   if array.length >= min_size || min_size == 0
+     return array.clone
+   else
+     final_array = array.clone.fill(value, array.length...min_size)
+   end
+   return final_array
+ end
+
+
+
+
 
 
 
