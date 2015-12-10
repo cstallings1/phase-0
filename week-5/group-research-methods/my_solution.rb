d493279bc6b61814f4493553ec49ace94b7edff1
cstallings1 <catiestallings1@gmail.com>
2015-12-10 11:56:34 -0600

    Refactor solutions

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -7,26 +7,17 @@ my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, 
 
 # Person 1's solution
 def my_array_finding_method(source, thing_to_find)
-  filtered_array = []
-  source.each do |word|
-    if word.to_s.include?(thing_to_find)
-      filtered_array << word
-    end
-  end
-  return filtered_array
+  source.find_all {|i| i.to_s.include?(thing_to_find)}
 end
 
 
 def my_hash_finding_method(source, thing_to_find)
-  filtered_array = source.select {|k,v| v == thing_to_find}.keys
-
-  return filtered_array
+  source.select {|k,v| v == thing_to_find}.keys
 end
 
 # Identify and describe the Ruby method(s) you implemented.
 #.include? to check if an array element included a given letter
-#.each to iterate thorugh an array.
-#I used the "<<" form of push to add an element to an array.
+#.find_all to select all items of an array that matched a given criteria
 #.select was used to select a hash value that met a given criteria.
 #.keys was used to access hash keys as an array.
 
@@ -92,7 +83,10 @@ end
 
 # Release 1: Identify and describe the Ruby method you implemented. Teach your
 # accountability group how to use the methods.
-#.include? is a great way to check if a string includes a given character(s). It is a string method so you would need to convert any Fixnums to a string using .to_s before applying .include?. When using this method you'll most likely be pushing the array element that meets the criteria into a new filtered array.
+
+#.find_all will iterate through an array and find all elements that match the criteria. The elements that match are returned in an array.
+
+#.include? is a great way to check if a string includes a given character(s). It is a string method so you would need to convert any Fixnums to a string using .to_s before applying .include?.
 
 #The .select method returns a new hash that meets a given criteria. In my example, the value had to equal a given number for it to return true and the element to be added to the new hash. Because the instructions asked for a new array of hash keys, I had to apply .keys at the end which creates a new array of only the hash keys.
 
