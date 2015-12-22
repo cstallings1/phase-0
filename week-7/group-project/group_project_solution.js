20f9f369400b0ecfea146fa3e126eefb254ed11e
cstallings1 <catiestallings1@gmail.com>
2015-12-22 10:15:47 -0600

    Add initial files for challenge 7.8

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,5 @@
+//User Stories:
+
+  //1. As a user I want a function called 'sum' that will provide the sum of a given set of numbers.
+  //2. As a user I want a function called 'mean' that will provide the average of a given set of numbers.
+  //3. As a user I want a function called 'median' that will provide the middle value of a given set of sorted numbers. If the given set of numbers contains an even amount of integers, I want the average of the two middle numbers.
