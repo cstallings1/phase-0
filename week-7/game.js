b48f3422cf92e86e280e7f8d15a522f98599eef8
cstallings1 <catiestallings1@gmail.com>
2015-12-23 13:45:23 -0600

    Add initial solution

--- week-7/game.js
+++ week-7/game.js
@@ -13,7 +13,7 @@
   //Martian
 // Objects:
   //Astronaut
-    //Properties: x coordinate, y coordinate, shoot, move
+    //Properties: x coordinate, y coordinate, win, shoot, move
   //Martian
     //Properties: x coordinate, y coordinate
   //Spaceship
@@ -43,15 +43,73 @@
 //Create the martian object
   //Add x and y coordinates as propeties set to 0
 //Create the spaceship object
-  //Add x and y coordinates as propeties set to 0
+  //Add x and y coordinates as propeties set to random numbers
 //Create the raygun object
   //Add x and y coordinates as propeties set to 0
 
 
 // Initial Code
+var astronaut = {
+  x: 0,
+  y: 0,
+  //Moves astronaut and martian
+  move: function(direction) {
+    if (direction === "right") {
+      this.x += 2;
+    } else if (direction === "left") {
+      this.x -= 2;
+    } else if (direction === "up") {
+      this.y += 2;
+    } else if (direction === "down") {
+      this.y -= 2;
+    } else {
+      console.log("That was not a valid direction")
+    };
+    console.log("astronuat is at " + this.x + "," + this.y);
+    martian.x = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+    martian.y = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+    console.log("martian is at " + martian.x + "," + martian.y);
+    //Check if astronaut made it to the ship
+    if ((this.x === spaceship.x) && (this.y === spaceship.y)) {
+      console.log("You made it back to the ship safe and sound.")
+    };
+    //Check if martian captured astronaut
+    if ((this.x === martian.x) && (this.y === martian.y)) {
+      console.log("The martian captured you GAME OVER")
+    };
+  },
+  //Fires the raygun by changing it's position
+  shoot: function() {
+    raygun.x = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+    raygun.y = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+    console.log("raygun shoots at " + raygun.x + "," + raygun.y);
+    if ((raygun.x === martian.x) && (raygun.y === martian.y)) {
+      console.log("Congratulations you killed the martian! Now you can safely go back to the spaceship.")
+    };
+  }
+};
 
+var martian = {
+  x: 0,
+  y: 0
+};
 
+var spaceship = {
+  x: Math.floor(Math.random() * (5 - 1 + 1)) + 1,
+  y: Math.floor(Math.random() * (5 - 1 + 1)) + 1
+};
 
+var raygun = {
+  x: 0,
+  y: 0
+};
+
+astronaut.move("right");
+astronaut.shoot();
+astronaut.move("left");
+astronaut.shoot();
+astronaut.move("up");
+astronaut.shoot();
 
 
 
