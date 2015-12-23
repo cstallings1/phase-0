0fdd3fd1f0df888cf13e1e13e2418307bca407ae
cstallings1 <catiestallings1@gmail.com>
2015-12-23 12:00:33 -0600

    Add mission outline and pseudocode

---
+++ week-7/game.js
@@ -0,0 +1,73 @@
+// Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description:
+// Overall mission:
+  //You're an astronaut and you need to get back to the spaceship. There is a martian trying to stop you so you have to shoot at him as you make your way back to safety.
+// Goals:
+  //Get back to spaceship while avoiding the martian
+  //Shoot the martian
+// Characters:
+  //Astronaut
+  //Martian
+// Objects:
+  //Astronaut
+    //Properties: x coordinate, y coordinate, shoot, move
+  //Martian
+    //Properties: x coordinate, y coordinate
+  //Spaceship
+    //Properties: x coordinate, y coordinate
+  //Raygun
+    //Properties: x coordinate, y coordinate
+// Functions:
+  //Move astronaut
+    //This will change his coordinates
+    //If his coordiantes are the same as the martian, he dies
+    //If his coordinates are the same as the sapceship, he wins
+  //Shoot raygun
+    //This changes the raygun coordinates
+    //If the raygun coordinates are the same as the martian you win
+  //Move martial
+    //This will change his coordinates
+
+// Pseudocode
+//Create the astronaut object
+  //Add properties:
+    //Add x and y coordinates as propeties set to 0
+    //shoot: a function that changes the x and y coordinates of the raygun
+      //IF the raygun coordinates are the same as the martian - you win
+    //move: a function that changes x and y coordinates of the astronaut and the martian
+      //IF the coordinates are the same as the martians - you lose
+      //IF the coordinates are the same as the spaceship - you win
+//Create the martian object
+  //Add x and y coordinates as propeties set to 0
+//Create the spaceship object
+  //Add x and y coordinates as propeties set to 0
+//Create the raygun object
+  //Add x and y coordinates as propeties set to 0
+
+
+// Initial Code
+
+
+
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//
