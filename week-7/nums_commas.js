45b9761c155fe3becb59638fc7c92cee5e4eb119
cstallings1 <catiestallings1@gmail.com>
2015-12-24 11:16:45 -0600

    Add refactored solution and reflection

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -14,24 +14,32 @@
 // Reverse string for solution
 
 
-
 // Initial Solution
+// var numComma = function(number) {
+//   number = number.toString();
+//   number = number.split("");
+//   number.reverse();
+
+//   var new_array = [];
+
+//   for (var i = 0; i < number.length; i++ ) {
+//     if ((i % 3 == 0) && (i != 0)) {
+//       new_array.push(",");
+//     }
+//     new_array.push(number[i]);
+//   };
+
+//   new_array.reverse();
+//   return (new_array.join(''));
+// };
+
+// Refactored Solution
 var numComma = function(number) {
-  number = number.toString();
-  number = number.split("");
-  number.reverse();
-
-  var new_array = [];
-
-  for (var i = 0; i < number.length; i++ ) {
-    if ((i % 3 == 0) && (i != 0)) {
-      new_array.push(",");
-    }
-    new_array.push(number[i]);
-  };
-
-  new_array.reverse();
-  return (new_array.join(''));
+  number = number.toString().split("").reverse();
+  for (var i = 3; i < number.length; i+=4 ) {
+    number.splice(i, 0, ",");
+  }
+  return number.reverse().join('');
 };
 
 // Your Own Tests (OPTIONAL)
@@ -46,83 +54,43 @@ function assert(test, message, test_number) {
 
 assert(
   (numComma(0) === '0'),
-  "The value of numComma(0) should be '0'.",
+  "The value of numComma(0) should be '0'. You returned " + numComma(0),
   "1. "
 )
 
 assert(
   (numComma(123) === '123'),
-  "The value of numComma(123) should be '123'.",
-  "1. "
+  "The value of numComma(123) should be '123'. You returned " + numComma(123),
+  "2. "
 )
 
 assert(
   (numComma(1000) === '1,000'),
-  "The value of numComma(1000) should be '1,000'.",
-  "1. "
+  "The value of numComma(1000) should be '1,000'. You returned " + numComma(1000),
+  "3. "
 )
 
 assert(
   (numComma(237874832) === '237,874,832'),
-  "The value of numComma(237874832) should be '237,874,832'.",
-  "1. "
+  "The value of numComma(237874832) should be '237,874,832'. You returned " + numComma(237874832),
+  "4. "
 )
 
-
-// Final Refactored Solution
-var numComma = function(number) {
-  number = number.toString().split("").reverse();
-  var commaAry = [];
-  for (var i = 0; i < number.length; i++ ) {
-    if ((i % 3 == 0) && (i != 0)) {
-      commaAry.push(",");
-    }
-    commaAry.push(number[i]);
-  }
-  commaAry.reverse();
-  return (commaAry.join(''));
-};
-
-
-//Solution using strings only
-//We thought it might be a cleaner solution to use strings
-//instead of arrays but it didn't turn out that way.
-
-function reverse(x) {
-  x = x.toString();
-  var i = (x.length - 1);
-  var newStr = '';
-  while (i > -1) {
-    newStr += (x[i]);
-    i --;
-  };
-  return newStr;
-};
-
-var numComma = function(num) {
-  num = reverse(num);
-  var commaStr = "";
-
-  for (var i = 0; i < num.length; i++ ) {
-    if ((i % 3 == 0) && (i != 0)) {
-      commaStr += (",");
-    };
-    commaStr += (num[i]);
-  };
-  commaStr = reverse(commaStr);
-  return(commaStr);
-};
-
+assert(
+  (numComma(10000) === '10,000'),
+  "The value of numComma(10000) should be '10,000'. You returned " + numComma(10,000),
+  "5. "
+)
 
 
 // Reflection
 // What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently?
   //It was actually pretty similar in terms of breaking the problem down. However, I think we were forced to break down the problem into even smaller steps without the help of some of Ruby's built-in methods. We did have to approach the problem differently when it came to figuring out how to actually iterate through the array and insert the comma.
 // What did you learn about iterating over arrays in JavaScript?
-  //I learned that you can use the incrementor as a step instead of just incrementing by 1 each time - I didn't end up using this in the solution but I can see how that will helpful in the future.
+  //I learned that you can use the incrementor as a step instead of just incrementing by 1 each time.
 // What was different about solving this problem in JavaScript?
-  //The main differnce was figuring out how to solve the problem without as many built in methods. In one of our solutions we ended up having to write our own reverse method because you can't call reverse on a string.
+  //The main differnce was figuring out how to solve the problem without as many built in methods. At one point we wrote our own reverse function to reverse a string but that didn't end up being a good solution.
 // What built-in methods did you find to incorporate in your refactored solution?
-  //We didn't end up using any new methods in our refactored version. We mainly ended up refactoring for better variable names, and conciseness. We did end up chaining some methods together in our refactored solution, I learned that you can do that in JavaScript as well.
+  //We ended up using the splice method to insert the comma into our array - ths was a new on for me and I can see it being really helpful since we don't have an insert function like we did in Ruby.
 
 
