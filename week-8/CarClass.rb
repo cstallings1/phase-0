7f1e600dd7f8373c3404112143ce19ef0e743166
cstallings1 <catiestallings1@gmail.com>
2016-01-08 15:48:50 -0600

    Add pseudocode

---
+++ week-8/CarClass.rb
@@ -0,0 +1,74 @@
+# Create a Car Class from User Stories
+
+
+# I worked on this challenge [by myself].
+
+
+# 2. Pseudocode
+#Create a car object
+#Create an initialize method:
+  #Input: model, color, distance
+  #Output: none
+  #Steps:
+    #Create a variable for car, model, and distance
+    #Create a variable for speed
+#Create a speed method
+  #Input: none
+  #Output: current speed
+  #Steps:
+    #Return the current speed variable
+#Create a turn method
+  #Input: right or left
+  #Output: statement telling you which way you turned
+  #Steps:
+    #Return a statement telling the player which way they have turned.
+#Create an accelerate method
+  #Input: desired speed
+  #Output: statement telling you your current speed
+  #Steps:
+    #Add difference between desired speed and current speed to current speed
+    #Return current speed
+#Create an decelerate method
+  #Input: desired speed
+  #Output: statement telling you your current speed
+  #Steps:
+    #Subtract difference between desired speed and current speed from current speed
+    #Return current speed
+#Create a distance traveled method
+  #Input: none
+  #Output: Return the total distance traveled
+  #Steps:
+    #Return the distance variable
+#Create a stop method
+  #Input: none
+  #Output: statement telling you you've stopped
+  #Steps:
+    #Set speed variable to 0
+    #Return a statement saying you've stopped the car
+
+
+
+
+
+
+
+# 3. Initial Solution
+
+
+
+
+# 4. Refactored Solution
+
+
+
+
+
+
+# 1. DRIVER TESTS GO BELOW THIS LINE
+
+
+
+
+
+
+# 5. Reflection
