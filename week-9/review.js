0ebd92db25c1641f3d0b25ef0da5e94c00ad1313
cstallings1 <catiestallings1@gmail.com>
2016-01-11 17:18:56 -0600

    Add pseudocode

--- week-9/review.js
+++ week-9/review.js
@@ -5,3 +5,38 @@
 // As a user I want to be able to update quantities for an item in my list
 // As a user I want to be able to print the list
 
+// Release 2 Pseudocode:
+// Create an empty grocery list that contains entries of items and a quantity
+// Create an 'add item' function that allows user to add an item and quantity to list
+// INPUT: item and quantity
+// OUTPUT: none
+// STEPS:
+//   Evaluate each item in the grocery list
+//   IF item is not already in the grocery list
+//     Add the item and associated quantity
+//   END
+
+// Create a 'remove item' function that allows user to remove an item
+// INPUT: item
+// OUTPUT: none
+// STEPS:
+//   Evaluate each item in the list
+//   IF item is in the list
+//     Remove the item from list
+//   END
+
+// Create an 'update' function that allows user to update quantity
+// INPUT: item and quantity
+// OUTPUT: none
+// STEPS
+//   Evaluate each item in the list
+//   WHEN you find the given item
+//     change the original quantity to the new quantity
+//   END
+
+// Create a 'print' functio that prints the list
+// INPUT: none
+// OUTPUT: a printed list
+// STEPS:
+//  Evaluate each item in the list and print out the item and quantity
+
