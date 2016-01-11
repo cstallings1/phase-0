a033ad021d642fe1af0f7f82d0811a6ccecaecee
cstallings1 <catiestallings1@gmail.com>
2016-01-11 17:10:50 -0600

    Add review.js and add user stories

---
+++ week-9/review.js
@@ -0,0 +1,7 @@
+//Release 1 Add user stories:
+// As a user I want to be able to create a new grocery list
+// As a user I want to add an item with a quantity to the list
+// As a user I want to be able to remove an item from the list
+// As a user I want to be able to update quantities for an item in my list
+// As a user I want to be able to print the list
+
