43c7e498df1828a9e9089978e4b579f945b760b0
cstallings1 <catiestallings1@gmail.com>
2016-01-12 11:00:00 -0600

    Add initial files for ruby-review solo challenge

---
+++ week-9/ruby-review-2/ruby_review.rb
@@ -0,0 +1,25 @@
+# I worked on this challenge [by myself, with: ].
+# This challenge took me [#] hours.
+
+
+# Pseudocode
+
+
+
+# Initial Solution
+
+def super_fizzbuzz(array)
+
+
+end
+
+
+
+# Refactored Solution
+
+
+
+
+
+
+# Reflection
