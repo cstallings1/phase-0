daa0eeec758547590fe020370e06d9fd8a18f50d
cstallings1 <catiestallings1@gmail.com>
2016-01-12 11:03:59 -0600

    Add pseudocode

--- week-9/ruby-review-2/ruby_review.rb
+++ week-9/ruby-review-2/ruby_review.rb
@@ -1,10 +1,23 @@
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 # This challenge took me [#] hours.
 
 
 # Pseudocode
-
-
+# Create method called 'super_fizzbuzz'
+# INPUT: array of numbers
+# OUTPUT: array of the same numbers, except multiples of 3 replaced with 'Fizz', multiples of 5 replaced with 'Buzz' and multiples of both replaced with 'FizzBuzz'.
+# STEPS:
+# Create a new empty container
+# Evaluate each number in original array
+# IF the number is divisible by 5 and 3
+#   Add 'FizzBuzz' to new container
+# ELSIF the number is divisible by 3
+#   Add 'Fizz' to the new container
+# ELSIF the number is divisible by 5
+#   Add 'Buzz' to the new container
+# ELSE
+#   Add the number to the container
+# Return the new container
 
 # Initial Solution
 
