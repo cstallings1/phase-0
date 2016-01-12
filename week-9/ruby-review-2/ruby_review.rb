5cb091dcdc7042687853910311b1bba45ffe5bdf
cstallings1 <catiestallings1@gmail.com>
2016-01-12 11:38:19 -0600

    Add reflection

--- week-9/ruby-review-2/ruby_review.rb
+++ week-9/ruby-review-2/ruby_review.rb
@@ -1,5 +1,5 @@
 # I worked on this challenge [by myself].
-# This challenge took me [#] hours.
+# This challenge took me [40] min.
 
 
 # Pseudocode
@@ -21,29 +21,45 @@
 
 # Initial Solution
 
-def super_fizzbuzz(array)
-  fizzbuzzed_array = []
-  array.each do |n|
-    if (n % 3 == 0) && (n % 5 == 0)
-      fizzbuzzed_array.push('FizzBuzz')
-    elsif n % 3 == 0
-      fizzbuzzed_array.push('Fizz')
-    elsif n % 5 == 0
-      fizzbuzzed_array.push('Buzz')
-    else
-      fizzbuzzed_array.push(n)
-    end
-  end
-  return fizzbuzzed_array
-end
-
-
+# def super_fizzbuzz(array)
+#   fizzbuzzed_array = []
+#   array.each do |n|
+#     if (n % 3 == 0) && (n % 5 == 0)
+#       fizzbuzzed_array.push('FizzBuzz')
+#     elsif n % 3 == 0
+#       fizzbuzzed_array.push('Fizz')
+#     elsif n % 5 == 0
+#       fizzbuzzed_array.push('Buzz')
+#     else
+#       fizzbuzzed_array.push(n)
+#     end
+#   end
+#   return fizzbuzzed_array
+# end
 
 # Refactored Solution
+def super_fizzbuzz(array)
+  array.map do |n|
+    if n % 15 == 0
+      'FizzBuzz'
+    elsif n % 3 == 0
+      'Fizz'
+    elsif n % 5 == 0
+      'Buzz'
+    else
+      n
+    end
+  end
+end
 
-
-
+puts super_fizzbuzz([30, 9, 20, 1]) # => ["FizzBuzz", "Fizz", "Buzz", 1]
 
 
 
 # Reflection
+# What concepts did you review in this challenge?
+  # I reviewed iterating over an array and using.map. It was helpful to review how .each and .map are different in that .map returns a new array. I also reviewed case statements and if statements but ended up using the if statement.
+# What is still confusing to you about Ruby?
+  # There really wasn't anything that confused me about this problem. I just need to keep the built in Ruby methods fresh in my mind for Phase-1.
+# What are you going to study to get more prepared for Phase 1?
+  # I will study tbe different enumerable methods by reading through the Ruby docs to keep this fresh in my mind.
